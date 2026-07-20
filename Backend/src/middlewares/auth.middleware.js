20a123582f4d98a973d785b98a89c9f4784012a5
iqrakhatoon-dev <iqrakhatoonnn@gmail.com>
2026-07-20 22:09:10 +0530

    fix blacklisting

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,45 +1,33 @@
+const jwt = require("jsonwebtoken");
+const tokenBlacklistModel = require("../models/blacklist.model");
+
 async function authUser(req, res, next) {
-
-  console.log("Cookies:", req.cookies);
-
   const token = req.cookies.token;
 
-  console.log("Token:", token);
-
   if (!token) {
-    console.log("NO TOKEN");
     return res.status(401).json({
-      message: "Access denied. No token provided."
+      message: "Access denied. No token provided.",
     });
   }
 
   const isTokenBlacklisted = await tokenBlacklistModel.findOne({
-    token
+    token,
   });
-
-  console.log("Blacklisted:", !!isTokenBlacklisted);
 
   if (isTokenBlacklisted) {
     return res.status(401).json({
-      message: "Token is invalid"
+      message: "Token is invalid",
     });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log("Decoded:", decoded);
-
     req.user = decoded;
-
     next();
-
   } catch (err) {
-
-    console.log("JWT ERROR:", err.message);
-
     return res.status(401).json({
-      message: "Invalid token."
+      message: "Invalid token.",
     });
   }
 }
