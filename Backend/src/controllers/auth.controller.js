343a97ebb39d6e4bf691f77dc0aac250bc0fd363
iqrakhatoon-dev <iqrakhatoonnn@gmail.com>
2026-07-20 22:01:17 +0530

    fixed GET ME

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -145,6 +145,13 @@ async function logoutUserController(req, res) {
  */
 async function getMeController(req, res) {
   const user = await userModel.findById(req.user.id);
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found"
+    });
+  }
+
   res.status(200).json({
     message: "User details fetched successfully",
     user: {
