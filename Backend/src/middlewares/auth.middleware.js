e9f15019aa4e9a9a2ecb55756bda1dbd847e04f3
iqrakhatoon-dev <iqrakhatoonnn@gmail.com>
2026-07-20 21:04:42 +0530

    Initial commit

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,30 +1,45 @@
-const jwt = require("jsonwebtoken");
-const tokenBlacklistModel = require("../models/blacklist.model");   
-
 async function authUser(req, res, next) {
+
+  console.log("Cookies:", req.cookies);
+
   const token = req.cookies.token;
 
+  console.log("Token:", token);
+
   if (!token) {
-    return res.status(401).json({ message: "Access denied. No token provided." });
+    console.log("NO TOKEN");
+    return res.status(401).json({
+      message: "Access denied. No token provided."
+    });
   }
 
   const isTokenBlacklisted = await tokenBlacklistModel.findOne({
     token
-  })
+  });
 
-  if(isTokenBlacklisted){
+  console.log("Blacklisted:", !!isTokenBlacklisted);
+
+  if (isTokenBlacklisted) {
     return res.status(401).json({
-        message: 'Token is invalid'
-    })
+      message: "Token is invalid"
+    });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log("Decoded:", decoded);
+
     req.user = decoded;
+
     next();
+
   } catch (err) {
-    return res.status(401).json({ message: "Invalid token." });
+
+    console.log("JWT ERROR:", err.message);
+
+    return res.status(401).json({
+      message: "Invalid token."
+    });
   }
 }
-
-module.exports = { authUser };
